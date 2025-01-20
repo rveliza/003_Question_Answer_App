361ac2b461614f3bf6de604f8e6870e0dd4b0532
Reyner Veliz <reyner@reynerveliz.com>
2025-01-19 18:21:55 -0600

    Register user

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, g
+from flask import Flask, render_template, g, request
 from database import get_db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -14,8 +15,20 @@ def index():
     return render_template('home.html')
 
 
-@app.route('/register')
+@app.route('/register', methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        db = get_db()
+
+        name = request.form['name']
+        password = request.form['password']
+        hashed_password = generate_password_hash(password)
+
+        db.execute("INSERT INTO users (name, password, expert, admin) VALUES (?,?,?,?)", [name, hashed_password, 0, 0])
+        db.commit()
+        return "User Created!"
+
+        return f"Password: {hashed_password}"
     return render_template('register.html')
 
 
