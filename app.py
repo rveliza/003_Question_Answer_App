30125f62053320292de96d4981146c3fc85ed06a
Reyner Veliz <reyner@reynerveliz.com>
2025-01-21 07:52:25 -0600

    Updating Links

--- app.py
+++ app.py
@@ -27,10 +27,6 @@ def get_current_user():
 @app.route('/')
 def index():
     user = get_current_user()
-    print(f"user: {user}")
-    if 'user' in session:
-        user = session['user']
-
     return render_template('home.html', user=user)
 
 
@@ -48,7 +44,7 @@ def register():
         db.commit()
         return "User Created!"
 
-    return render_template('register.html')
+    return render_template('register.html', user=user)
 
 
 @app.route('/login', methods=["GET", "POST"])
@@ -71,37 +67,37 @@ def login():
         else:
             return "Password incorrect"
     
-    return render_template('login.html')
+    return render_template('login.html', user=user)
 
 
 @app.route('/question')
 def question():
     user = get_current_user()
-    return render_template('question.html')
+    return render_template('question.html', user=user)
 
 
 @app.route('/answer')
 def answer():
     user = get_current_user()
-    return render_template('answer.html')
+    return render_template('answer.html', user=user)
 
 
 @app.route('/ask')
 def ask():
     user = get_current_user()
-    return render_template('ask.html')
+    return render_template('ask.html', user=user)
 
 
 @app.route('/unanswered')
 def unanswered():
     user = get_current_user()
-    return render_template('unanswered.html')
+    return render_template('unanswered.html', user=user)
 
 
 @app.route('/users')
 def users():
     user = get_current_user()
-    return render_template('users.html')
+    return render_template('users.html', user=user)
 
 
 @app.route('/logout')
